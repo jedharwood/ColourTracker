ca1b7efbd8781cec39f1fb923f1dc4fee2bf8b72
Jed Harwood <scaryteeth@hotmail.com>
2020-04-04 21:21:59 +0100

    Basic functionality
    Scaffolding out basic functionality

---
+++ ColourTrackerApplication/Startup.cs
@@ -0,0 +1,65 @@
+using JavaScriptEngineSwitcher.ChakraCore;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using React.AspNet;
+
+namespace ColourTrackerApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddReact();
+
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName)
+              .AddChakraCore();
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+
+            app.UseReact(config =>
+            {});
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ ColourTrackerApplication/wwwroot/js/getAllAndPostColours.jsx
@@ -0,0 +1,29 @@
+﻿class ColourDisplay extends React.Component {
+    render() {
+        return (
+            <div className="colourDisplay">
+                <h1>Colours</h1>
+                <ColourList />
+                <AddColourForm />
+            </div>
+        );
+    }
+}
+
+class ColourList extends React.Component {
+    render() {
+        return (
+            <div className="colourList">Hello, world! I am a ColourList.</div>
+        );
+    }
+}
+
+class AddColourForm extends React.Component {
+    render() {
+        return (
+            <div className="addColourForm">Add a colour to your list</div>
+        );
+    }
+}
+
+ReactDOM.render(<ColourDisplay />, document.getElementById('content'),);
